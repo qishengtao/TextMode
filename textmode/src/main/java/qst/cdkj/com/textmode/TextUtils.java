f0eb8fc1ccef375988a3207a70201209fe3c6fbf
15968105952 <1824902907@qq.com>
2018-06-07 17:54:54 +0800

    首次提交

---
+++ textmode/src/main/java/qst/cdkj/com/textmode/TextUtils.java
@@ -0,0 +1,13 @@
+package qst.cdkj.com.textmode;
+
+/**
+ * @author qi
+ * @updateDts 2018/6/7
+ */
+
+public class TextUtils {
+
+    public static int jia(int a, int b) {
+        return a + b;
+    }
+}
